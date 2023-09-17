c39c28865bd813c62d049787c896096ddb3dc5c2
jadaan96 <jadaan.2009@gmail.com>
2023-09-17 16:36:21 +0000

    adding cart

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,139 @@
-import React from 'react'
-import AppBar from '@mui/material/AppBar';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import CameraIcon from "@mui/icons-material/PhotoCamera";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { connect } from "react-redux";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { CART_REMOVE } from "../../Store/Actions";
 
-function Header() {
+function Header(props) {
+  const [state, setState] = useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  // Maintain a separate state for item quantities
+  const [itemQuantities, setItemQuantities] = useState({});
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const handleQuantityChange = (itemId, change) => {
+    setItemQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [itemId]: (prevQuantities[itemId] || 0) + change,
+    }));
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{
+        width: anchor === "top" || anchor === "bottom" ? "auto" : 250,
+      }}
+      role="presentation"
+      // onClick={toggleDrawer(anchor, false)} Remove this line to prevent closing on click
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {props.Cart.map((item) => (
+          <ListItem key={item.id} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <DeleteIcon
+                  onClick={() => {
+                    props.CART_REMOVE(item.name);
+                  }}
+                  style={{ cursor: "pointer", color: "tomato" }}
+                />
+              </ListItemIcon>
+              <ListItemText primary={item.name} />
+              <ListItemIcon>
+                <Button
+                  onClick={() => handleQuantityChange(item._id, -1)}
+                  startIcon={<RemoveIcon />}
+                  size="small"
+                  disabled={(itemQuantities[item._id] || 0) === 0}
+                />
+                {itemQuantities[item._id] || 0}
+                <Button
+                  onClick={() => handleQuantityChange(item._id, 1)}
+                  startIcon={<AddIcon />}
+                  size="small"
+                />
+              </ListItemIcon>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
     <div>
-       <CssBaseline />
+      <CssBaseline />
       <AppBar position="relative">
-        <Toolbar>
-          <CameraIcon sx={{ mr: 2 }} />
+        <Toolbar style={{ display: "flex", alignItems: "center" }}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginRight: "80%",
+            }}
+          >
+            <CameraIcon sx={{ marginRight: 1 }} />
+            <Typography variant="h6" color="inherit" noWrap>
+              jadaan Store
+            </Typography>
+          </div>
+          <ShoppingCartIcon
+            sx={{ marginRight: 2, cursor: "pointer" }}
+            onClick={toggleDrawer("right", true)}
+          />
           <Typography variant="h6" color="inherit" noWrap>
-            Album layout
+            {props.Cart.length}
           </Typography>
         </Toolbar>
       </AppBar>
+      <Drawer
+        anchor="right"
+        open={state["right"]}
+        onClose={toggleDrawer("right", false)}
+      >
+        {list("right")}
+      </Drawer>
     </div>
-  )
+  );
 }
 
-export default Header
+const mapStateToProps = (state) => ({
+  Cart: state.cart.cart,
+});
+
+const mapDispatchToProps = { CART_REMOVE };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
