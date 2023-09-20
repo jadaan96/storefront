08d95c0b81a5bc6fecf268ca998519f6f32c11b0
jadaan96 <jadaan.2009@gmail.com>
2023-09-20 10:26:08 +0000

    lab 38

--- src/Store/cartReducer.js
+++ src/Store/cartReducer.js
@@ -1,23 +1,51 @@
+import axios from 'axios';
+
 const initialState = {
-    cart : [],
-  
+  cart: [],
+};
+
+export default (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case 'CART':
+      const existingItem = state.cart.find((item) => item.name === payload.name);
+      if (!existingItem) {
+        console.log(!existingItem);
+
+        axios
+          .put(`https://api-js401.herokuapp.com/api/v1/products/${payload._id}`, {
+            inStock: payload.inStock - 1,
+          })
+          .then((data) => {
+            console.log(data);
+          })
+          .catch((error) => {
+            console.error('Error updating product stock:', error);
+          });
+
+        return { ...state, cart: [...state.cart, payload] };
+      } else {
+        return { ...state, cart: [...state.cart] };
+      }
+
+    case 'CART_REMOVE':
+      axios
+        .put(`https://api-js401.herokuapp.com/api/v1/products/${payload._id}`, {
+          inStock: payload.inStock,
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error('Error resetting product stock:', error);
+        });
+
+      const removed = state.cart.filter((item) => item.name !== payload.name);
+
+      return { ...state, cart: removed };
+
+    default:
+      return state;
   }
-
-  export default (state = initialState, action) => {
-    const { type, payload } = action;
-  
-    switch (type) {
-      
-            case 'CART':
-                return { ...state, cart: [...state.cart, payload] };
-           case "CART_REMOVE":
-            const removed = state.cart.filter(item => item.name !== payload);
-            return { ...state, cart: removed };
-
-
-      default:
-        return state;
-    }
-  }
- 
- 
+};
--- src/Store/index.js
+++ src/Store/index.js
@@ -1,10 +1,11 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore,applyMiddleware } from "redux";
 import product from "./productReducer";
 import cart from "./cartReducer";
+import thunk from "redux-thunk";
 const reducers = combineReducers({ product,cart })
 
 const store = () => {
-  return createStore(reducers)
+  return createStore(reducers, applyMiddleware(thunk))
 }
 
 export default store();
